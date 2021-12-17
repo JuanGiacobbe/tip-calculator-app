26bde9c2dfe76cd37dae64b33bf0635252dfef55
Juan_Giacobbe <jgiacobbe32@gmail.com>
2021-12-17 15:46:59 -0300

    finished basic css and js

--- app.js
+++ app.js
@@ -3,14 +3,31 @@
 const bill = document.querySelector('.bill');
 const btns = document.querySelectorAll('.btn');
 const people = document.querySelector('.people');
+const tipText = document.querySelector('.tip-text');
+const totalText = document.querySelector('.total-text');
 
+// EVENTS
 btns.forEach(function(btn){
     btn.addEventListener('click', (e)=>{
         e.preventDefault();
 
-
+        // OPERATIONS
         let tip = (parseInt(bill.value) * (parseInt(btn.value)/100));
+        let tipPeople = tip / people.value;
         let total = parseInt(bill.value) + tip;
-        console.log(total);
+        let totalPeople = total / people.value;
+
+        // DOM MODIFICATIONS
+        let tipLine = document.createElement('p');
+        tipLine.textContent = tipPeople;
+        let totalLine = document.createElement('p');
+        totalLine.textContent = totalPeople;
+
+        tipText.appendChild(tipLine);
+        totalText.appendChild(totalLine);
+
+        bill.value = '';
+        people.value = '';
+        btn.classList.add('active');
     })
 })
